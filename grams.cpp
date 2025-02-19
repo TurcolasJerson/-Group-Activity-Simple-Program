8098c6af63016a1e2bfa3846361263a46c0c6dee
Turcolas, Jerson S. <turcolasjerson_bsit@plmun.edu.ph>
2025-02-19 19:06:46 +0800

    Create grams.cpp

---
+++ grams.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+double pounds;
+
+cout << "Enter A Number You Want To Convert: ";
+cin >> pounds;
+double grams = pounds * .0022;
+cout << "It Converted To: "<<grams;
+
+return 0;
+}
