579d23d58e9baba5426b4d55e45de6e60ffb99f9
Turcolas, Jerson S. <turcolasjerson_bsit@plmun.edu.ph>
2025-02-19 19:11:29 +0800

    Create Circle.cpp

---
+++ Circle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+double area;
+
+cout << "Enter The Area Of A Circle: ";
+cin >> area;
+double circle = 3.14 * (area * area /2);
+cout << "The Area Is: "<<circle;
+
+return 0;
+}
